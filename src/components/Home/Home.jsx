5861e58e1091cd4b5e9d93e6d80769422047e07c
JohanRomero95 <johanarm95@gmail.com>
2024-02-23 15:32:46 -0300

    modificando formato videos

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import pauseImg from "../../assets/svg/pause.svg";
 import Slider from "../Slider/Slider";
 import PublicMid from "../Promo/PublicHoliday/PublicMid";
 import PublicRunning from "../Promo/PublicRunning/PublicRunning";
-import VideoRihanna from "../../assets/videos/videoPumaRihanna.webm";
+import VideoRihanna from "../../assets/videos/videoPumaRihanna (video-converter.com).mp4";
 import ButtonPrimary from "../Button/ButtonPrimary/ButtonPrimary";
 
 const Home = () => {
